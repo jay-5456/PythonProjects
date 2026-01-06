f62f2ba597a77830d50288da533a484dafe9fa84
Jayesh Naphade <jnaphade12@gmail.com>
2026-01-06 23:45:55 +0530

    Initial commit

---
+++ madlib_game.py
@@ -0,0 +1,19 @@
+print("🎉 Welcome to the Madlib Game 🎉")
+print("Kindly fill the words and Have fun at the end")
+place1=input("Enter a place name: ")
+adjective1=input("Enter a adjective: ")
+name1=input("Enter a person's name: ")
+verb1=input("Enter some verb: ")
+adjective2=input("Enter some another adjective: ")
+animal=input("Which is your favourite animal: ")
+verb2=input("Enter some another verb: ")
+emotion=input("Enter a sentiment: ")
+adjective3=input("Enter another adjective: ")
+print("\nEnjoy your Madlib Story")
+print(f"""\n\nCrazy Day at College !!!
+Today I went to the {place1} with my {adjective1} friend {name1}.
+We decided to {verb1} because it was very {adjective2} outside.
+Suddenly, a {animal} appeared and started {verb2} loudly.
+Everyone was {emotion}, but {name1} just laughed and said,
+"This is the most {adjective3} day of my life!"
+""")
